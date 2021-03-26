6fd3f0ef3995529556fd49b2ccc3f207e5015b30
Brayan <brayanleon25@hotmail.com>
2021-03-25 23:24:41 -0600

    recibir platos de pedido app

--- app_movil/src/app/services/objetos.service.ts
+++ app_movil/src/app/services/objetos.service.ts
@@ -152,6 +152,40 @@ export class ObjetosService {
     return lista_pedidos;
   }
 
+  public sacar_nombre_cantidad_pedido(N_pedido: number) {
+    var almacen: CarritoAlmacena[] = []
+    var car: Carrito = new Carrito
+    var platos_con_nombre = []
+    this.http.get<CarritoGenera>(this.Url + "Carrito_genera/npedido/" + N_pedido).subscribe(carrito_genera => {
+      this.http.get<Carrito[]>(this.Url + "Carrito").subscribe(carritos => {
+        for (let i = 0; i < carritos.length; i++) {
+          if (carritos[i].N_compra === carrito_genera.N_compra && carritos[i].Id === carrito_genera.Id_carrito) {
+            car = carritos[i];
+            break;
+          }
+        }
+        this.http.get<CarritoAlmacena[]>(this.Url + "Carrito_almacena/carrito/" + car.Id + "/" + car.N_compra)
+          .subscribe(carritos_almacena => {
+            almacen = carritos_almacena;
+
+            var platos: Plato[] = [];
+            this.http.get<Plato[]>(this.Url + "Plato").subscribe(data => {
+              platos = data;
+              platos.forEach(plato => {
+                almacen.forEach(dato_almacen => {
+                  if (plato.Numero_plato === dato_almacen.N_plato) {
+                    var plato_con_nombre = { cantidad: dato_almacen.Cantidad, nombre: plato.Nombre };
+                    platos_con_nombre.push(plato_con_nombre);
+                    console.log(plato_con_nombre);
+                  }
+                });
+              });
+            });
+          });
+      });
+    });
+    return platos_con_nombre;
+  }
 
   //obtiene el carrito de un pedido
   public obtener_carrito_pedido(N_pedido: number) {
@@ -186,9 +220,7 @@ export class ObjetosService {
           .subscribe(carritos_almacena => {
             almacen = carritos_almacena;
           });
-
-
-      })
+      });
     });
 
     return almacen;
