c3ad1a93f5f2086c48854200ec0bb677d4686e16
JUARSO <juan09fut.js@gmail.com>
2021-04-01 23:12:36 -0600

    Cambios Visaules 2

--- FrontEnd/restTEC_FrontEnd/src/app/Chef/vista-chef/vista-chef.component.ts
+++ FrontEnd/restTEC_FrontEnd/src/app/Chef/vista-chef/vista-chef.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { PedidosActivosService } from "../../pedidos-activos.service";
-import { Pedido } from "../../form-usuario/Comunicacion/pedido";
 
 @Component({
   selector: 'app-vista-chef',
@@ -8,12 +7,6 @@ import { Pedido } from "../../form-usuario/Comunicacion/pedido";
   styleUrls: ['./vista-chef.component.css']
 })
 export class VistaChefComponent implements OnInit {
-
-  public platos_con_nombre;
-  public todos_pedidos;
-  public sin_pedidos;
-  public pedidoActual;
-  public Cedula;
 
 
   /**
@@ -24,52 +17,5 @@ export class VistaChefComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.Cedula = this.pedidosActivosSistema.cedula;
-    this.pedidoActual = new Pedido;
-    this.pedidosActivosSistema.obtenerPedidos().subscribe(data => {
-      this.todos_pedidos = data;
-      this.sin_pedidos = data.filter(ped => (ped.Cedula_chef_asignado === this.Cedula) && ped.Estado === "Cocinando");
-      let pedidos_pedidos: Pedido[] =
-        data.filter(ped => (ped.Cedula_chef_asignado === this.Cedula) && ped.Estado.split(",")[0] === "pedir");
-      console.log(pedidos_pedidos);
-      if (pedidos_pedidos.length !== 0) {
-        console.log("hola");
-        alert("hay pedido pedido por " + pedidos_pedidos[0].Estado.split(",")[1]);
-      }
-    });
-
-  }
-
-  Ver_platos_pedido(pedido: Pedido): void {
-    this.pedidoActual = pedido;
-    this.platos_con_nombre = this.pedidosActivosSistema.sacar_nombre_cantidad(pedido.Numero);
-  }
-
-
-  rechazarpedido() {
-    this.pedidoActual.Cedula_chef_asignado = this.Cedula;
-    this.pedidoActual.Estado = "Cocinando"
-    this.pedidosActivosSistema.editarpedido(this.pedidoActual).subscribe(r => {
-
-      if (r === "registro editado exitosamente") {
-        this.terminaPlato();
-      }
-      this.ngOnInit();
-    });
-  }
-
-  reasignarpedido() {
-    this.pedidoActual.Cedula_chef_asignado = this.pedidoActual.Estado.split(",")[1] as number;
-    this.pedidoActual.Estado = "Cocinando"
-    this.pedidosActivosSistema.editarpedido(this.pedidoActual).subscribe(r => {
-
-      if (r === "registro editado exitosamente") {
-        this.terminaPlato();
-      }
-      this.ngOnInit();
-    });
-  }
-  terminaPlato(): void {
-    alert('El plato ha sido terminado y esta listo para recoger');
   }
 }
